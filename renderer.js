105d3d1d7e23a5bff36241cd3f8d2e22d2a3bd33
Anirudh Sundar <anirudhsundar98@github.com>
2016-12-11 13:34:17 +0530

    Added limits for zooming and panning

--- renderer.js
+++ renderer.js
@@ -2,7 +2,7 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
-var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+var requestAnimationFrame = window.requestAnimationFrame;
 
 var	width = window.innerWidth,
 	height = window.innerHeight;
@@ -22,27 +22,27 @@ var camera = {
 document.body.appendChild(renderer.view);
 
 document.body.addEventListener("keydown", function (e) {
-	if(e.keyCode == 37) {
+	if(e.keyCode == 37 && map.x + camera.x <= -10) {
 		camera.x += 10;
 	}
 		
-	if(e.keyCode == 38) {
+	if(e.keyCode == 38 && map.y + camera.y <= -10) {
 		camera.y += 10;
 	}	
 	
-	if(e.keyCode == 39) {
+	if(e.keyCode == 39 && map.x + camera.x >= width/camera.zoom - map.width + 10) {
 		camera.x -= 10;
 	}
 		
-	if (e.keyCode == 40) {
+	if (e.keyCode == 40 && map.y + camera.y >= height/camera.zoom - map.height + 10) {
 		camera.y -= 10;
 	}
 	
-	if(e.keyCode == 107) {
+	if(e.keyCode == 107 && camera.zoom < 2) {
 		camera.zoom *= 1.25;
 	}
 	
-	if(e.keyCode == 109) {
+	if(e.keyCode == 109 && camera.zoom > 0.5) {
 		camera.zoom /= 1.25;
 	}
 });
