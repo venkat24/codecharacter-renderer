937d8c43399974690bc82f827681783ac9cd8550
Anirudh Sundar <anirudhsundar98@github.com>
2017-01-19 23:46:27 +0530

    Added template for storyboard

--- src/buttons.js
+++ src/buttons.js
@@ -10,8 +10,8 @@ function exit() {
 
 		setTimeout(function() {
 			state = 1;
-			visiblitityChange();
 			endGame();
+			visiblitityChange();
 			menu.visible = true;
 		}, 500);
 	}
@@ -37,7 +37,7 @@ function restart() {
 
 		setTimeout(function() {
 			losState = 1;
-			document.getElementById('losImg').src = `assets/los${losState}.png`;
+			document.getElementById('losImg').src = `assets/los1.png`;
 
 			endGame();
 			loadGame();
@@ -54,18 +54,34 @@ function los() {
 		fade.style.zIndex = -10;
 		fade.style.opacity = 0;
 		document.getElementById('losImg').src = `assets/los${losState}.png`;
-		terrainVisibility = eval(`terrainVisibility${losState}`);
+		terrainVisibility = visibilityArray[losState];
 	}, 500);
 }
 
-function switchState() {
+function startStory() {
 	if (state == 1) {
 		fade.style.zIndex = 10;
 		fade.style.opacity = 1;
-		losState = 1;
-		document.getElementById('losImg').src = `assets/los${losState}.png`;
 
 		setTimeout(function() {
+			state = 2;
+			visiblitityChange();
+			menu.visible = false;
+			loadStory();
+		}, 500);
+	}
+}
+
+function startGame() {
+	if (state != 3) {
+		fade.style.zIndex = 10;
+		fade.style.opacity = 1;
+		losState = 1;
+		document.getElementById('losImg').src = `assets/los1.png`;
+
+		setTimeout(function() {
+			if (state == 2)
+				stage.removeChild(story);
 			state = 3;
 			visiblitityChange();
 			menu.visible = false;
@@ -80,6 +96,17 @@ function visiblitityChange() {
 		var menuButtons = document.getElementsByClassName('menu-button');
 		for (var i = 0; i < menuButtons.length; i++)
 			menuButtons[i].style.visibility = 'visible';
+
+		var gameButtons = document.getElementsByClassName('game-button');
+		for (var i = 0; i < gameButtons.length; i++)
+			gameButtons[i].style.visibility = 'hidden';
+
+	} else if (state == 2) {
+
+		document.getElementById('slide-in').style.visibility = 'hidden';
+		var menuButtons = document.getElementsByClassName('menu-button');
+		for (var i = 0; i < menuButtons.length; i++)
+			menuButtons[i].style.visibility = 'hidden';
 
 		var gameButtons = document.getElementsByClassName('game-button');
 		for (var i = 0; i < gameButtons.length; i++)
