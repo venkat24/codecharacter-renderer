4a458bde9c4c37a7c09d5dbe5b74c3c32ec769e2
Anirudh Sundar <anirudhsundar98@github.com>
2017-01-28 01:19:36 +0530

    Render actors from messages passed from a continous child output stream

--- src/test/test.js
+++ src/test/test.js
@@ -1,16 +1,25 @@
 var protobuf = require('protobufjs');
 var spawn = require('child_process').spawn;
 var child = spawn('./ipc');
+// var x;
 
-child.stdout.on('data', (data) => {
-	protobuf.load("state.proto", function(err, root) {
+function listen() {
+	console.log('listening');
+	child.stdout.on('data', (data) => {
+		console.log(data);
+		// x = data;
+		protobuf.load("state.proto", function(err, root) {
 
-		a = root.lookup("IPC.State");
-		decoded = a.decode(data);
-		for(var actor of decoded.actors)
-			for(var id in actor)
-				actor[id]=actor[id].toString();
-		console.log(decoded.actors);
+			a = root.lookup("IPC.State");
+			decoded = a.decode(data);
+			for(var actor of decoded.actors)
+				for(var id in actor)
+					actor[id]=actor[id].toString();
+			console.log(decoded.actors);
+			// listen();
+		});
 	});
-});
-child.on('close', (code) => console.log('Exit code: ' + code));
+}
+
+listen();
+// child.on('close', (code) => console.log('Exit code: ' + code));
